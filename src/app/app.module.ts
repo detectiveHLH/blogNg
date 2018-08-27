ae4f981a2d59bd0ccaccd6519c02275262cdd9ed
hulunhao <545759184@qq.com>
2018-08-27 11:13:55 +0800

    reinitial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,16 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import {RouterModule, Routes} from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavComponent } from './components/nav/nav.component';
-import { RowComponent } from './components/row/row.component';
-import { ColComponent } from './components/col/col.component';
-
-const appRoutes: Routes = [{
-  path: 'test',
-  component: AppComponent
-}, {
-  path: 'heroes',
-  component: HeroesComponent
-}];
-
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    FooterComponent,
-    NavComponent,
-    RowComponent,
-    ColComponent
+    AppComponent
   ],
   imports: [
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true }
-    ),
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
